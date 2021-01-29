6b5b5990ecefc9ca4aa51614fd047efa16fdfae2
ssoheeh <prinso@naver.com>
2021-01-29 21:32:18 +0900

    Create B2154.java
    01/29 백준 2154
    StringBuffer도 사용해보기
    반복문 안에서 계속 확인->메모리 초과의 원인

---
+++ B2154.java
@@ -0,0 +1,23 @@
+package Silver;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		String num = Integer.toString(n);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 1; i <= n; i++) {
+			sb.append(i);
+		}
+		bw.write(Integer.toString(sb.indexOf(num)+1));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
