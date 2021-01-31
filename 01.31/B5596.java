351571ac27ccdb9406b28e0ba5ef0fe07cc6fe4b
sohee <78138862+ssoheeh@users.noreply.github.com>
2021-01-31 21:44:45 +0900

    Update B5596.java

--- 01.31/B5596.java
+++ 01.31/B5596.java
@@ -8,19 +8,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
 		StringTokenizer st;
+		int s=0,t=0;
 		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[] book = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			book[i] = Integer.parseInt(st.nextToken());
+		while(st.hasMoreTokens()) {
+			s+=Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(book);
-		
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens()) {
+			t+=Integer.parseInt(st.nextToken());
+		}
+		if(s>t) {
+			bw.write(Integer.toString(s));
+		}else if(s<t) {
+			bw.write(Integer.toString(t));
+		}else {
+			bw.write(Integer.toString(s));	
+		}
 		bw.flush();
 		br.close();
 		bw.close();
 	}
-	
 }
