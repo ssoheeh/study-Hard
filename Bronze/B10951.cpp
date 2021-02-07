7f424aea447f8230a71d02a000dddcf8b2d6d6ef
ssoheeh <prinso@naver.com>
2021-02-07 12:15:39 +0900

    Create B10951.cpp

---
+++ Bronze/B10951.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int a, b;
+	while (cin>>a>>b) {
+		cout << a + b << endl;
+	}
+	return 0;
+}
