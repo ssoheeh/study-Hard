f49dfc35995435baa92e363850a19eedade886dc
ssoheeh <prinso@naver.com>
2021-02-09 11:49:15 +0900

    Create S11004.java

---
+++ Silver/S11004.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bw.write(Integer.toString(arr[k-1]));
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+	
+}
