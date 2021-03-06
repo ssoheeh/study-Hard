eba2d43a947b680beaf2d84c67eee852c7871d14
ssohee <prinso@naver.com>
2021-03-06 15:34:48 +0900

    Create S11931.java

---
+++ Silver/S11931.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for (int i = n-1; i >= 0; i--) {
+			bw.write(Integer.toString(arr[i]));
+			bw.newLine();
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
